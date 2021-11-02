b7e4c41b9f428562566d5d36f2554a23b514624f
Jonatan Morales <miky14n@gmail.com>
2021-11-02 16:20:00 -0400

    Test Vacio Fallando

---
+++ src/App.js
@@ -0,0 +1,6 @@
+let AllTask = Array();
+export default function CreateTask(nameTask)
+{
+    AllTask.push(nameTask);
+    return(nameTask);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './App';
+
+import CreateTask from "./App.js";
+const form = document.querySelector("#Organizador-form");
+const taskName = document.querySelector("#name-task");
+const direcciones = document.querySelector("#tipeTask");
+//const div = document.querySelector("#pos-final");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+   alert(CreateTask(String(taskName.value)));
+  //div.innerHTML = "<p>" + moverAuto( String( posIni+"/"+direcc)) + "</p>";
+  
+
+});
