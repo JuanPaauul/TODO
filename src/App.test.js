a9885ad5a35e08d7f43bbea11be6a3c4b3762434
P4NCH02798 <43188535+P4NCH02798@users.noreply.github.com>
2021-11-03 17:50:53 -0400

    Commit para la descripcion de la tarea

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
-import {returnEtiqueta} from "./App.js";
+import { returnEtiqueta } from "./App.js";
+import { returnDescripcion } from "./App.js";
 
 describe("Etiquetas ingresadas por usuario", () => {
     it("deberia retornar la etiqueta con #", () => {
-      expect(returnEtiqueta("etiqueta1")).toEqual("#etiqueta1");
+        expect(returnEtiqueta("etiqueta1")).toEqual("#etiqueta1");
     });
-  });
+});
+
+describe("Descripcion de la tarea", () => {
+    it("deberia retornar la descripcion", () => {
+        expect(returnEtiqueta("descripcion")).toEqual("descripcion");
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-
 import CreateTask from "./App.js";
 const form = document.querySelector("#Organizador-form");
 const taskName = document.querySelector("#name-task");
 const direcciones = document.querySelector("#tipeTask");
-const etiquetas=document.querySelector("#labelsTask");
+const etiquetas = document.querySelector("#labelsTask");
+const descripcion = document.querySelector("#descripcion");
 //const div = document.querySelector("#pos-final");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-   alert(CreateTask(String(taskName.value +" \nEtiquetas: " +etiquetas.value)));
-  //div.innerHTML = "<p>" + moverAuto( String( posIni+"/"+direcc)) + "</p>";
+    event.preventDefault();
+    alert(CreateTask(String(taskName.value + " \nEtiquetas: " + etiquetas.value + descripcion.value)));
+    //div.innerHTML = "<p>" + moverAuto( String( posIni+"/"+direcc)) + "</p>";
 });
