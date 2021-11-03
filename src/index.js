03f1a286ebec08ce7b370298790d302fee9b56bd
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-11-03 17:18:13 -0400

    Se agrego la opción para agregar fecha limite

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const form = document.querySelector("#Organizador-form");
 const taskName = document.querySelector("#name-task");
 const direcciones = document.querySelector("#tipeTask");
 const etiquetas=document.querySelector("#labelsTask");
+const fechaLimite = document.querySelector("#fecha-limite");
 //const div = document.querySelector("#pos-final");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-   alert(f.createTask(String(taskName.value +" \nEtiquetas: " +etiquetas.value)));
+    alert(f.createTask(String(taskName.value + " \nEtiquetas: " + etiquetas.value + " \nFecha Limite: " + fechaLimite.value)));
   //div.innerHTML = "<p>" + moverAuto( String( posIni+"/"+direcc)) + "</p>";
 });
