c7bf1be2dd41c005ae197c22053d31cdaec9177d
randyrafaelcg <randyrafaelcg@gmail.com>
2021-11-03 10:27:51 -0400

    Se agrego la opcion para las etiquetas

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import CreateTask from "./App.js";
 const form = document.querySelector("#Organizador-form");
 const taskName = document.querySelector("#name-task");
 const direcciones = document.querySelector("#tipeTask");
+const etiquetas=document.querySelector("#labelsTask");
 //const div = document.querySelector("#pos-final");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-   alert(CreateTask(String(taskName.value)));
+   alert(CreateTask(String(taskName.value +" \nEtiquetas: " +etiquetas.value)));
   //div.innerHTML = "<p>" + moverAuto( String( posIni+"/"+direcc)) + "</p>";
   
 
