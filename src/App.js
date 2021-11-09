3dd071bb0cc25e8d21c8f3bd53b4d7c732a8d09f
Juan Pablo Carrasco Padilla <JOMPOL_JP@hotmail.com>
2021-11-09 10:06:07 -0400

    update

--- src/App.js
+++ src/App.js
@@ -11,8 +11,26 @@ function returnEtiqueta(etiqueta)
     return "#etiqueta1"
 }
 
+export function insertNewTodoInTable(todoFormData){
+    let todoTableRef = document.getElementById("todo-table");
+    let newTodoRowRef = todoTableRef.insertRow(-1);
+    let newTypeCellRef = newTodoRowRef.insertCell(0);
+    let tableId = todoTableRef.rows.length - 1;
+    newTypeCellRef.textContent = todoTableRef.rows.length - 1;
+    newTypeCellRef = newTodoRowRef.insertCell(1);
+    newTypeCellRef.textContent = todoFormData.get("task-name")
+    newTypeCellRef = newTodoRowRef.insertCell(2);
+    newTypeCellRef.textContent = todoFormData.get("task-type")
+    newTypeCellRef = newTodoRowRef.insertCell(3);
+    newTypeCellRef.textContent = todoFormData.get("task-limit-date")
+    newTypeCellRef = newTodoRowRef.insertCell(4);
+    newTypeCellRef.textContent = todoFormData.get("task-tag")
+    newTypeCellRef = newTodoRowRef.insertCell(5);
+    newTypeCellRef.innerHTML = "<input type='checkbox' name='task-done-" + tableId + "'></input>";
+  }
+
 function getDate(date) {
     return new Date(date).toISOString().split('T')[0];
 }
 
-module.exports = {createTask, returnEtiqueta, getDate};
+module.exports = {createTask, returnEtiqueta, getDate, insertNewTodoInTable};
--- src/index.js
+++ src/index.js
@@ -6,26 +6,10 @@ var todoData = {};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  alert("asdad");
   let todoFormData = new FormData(form);
   todoFormData["task-id"]=todoIndex.toString();
-  insertNewTodoInTable(todoFormData, todoIndex);
+  f.insertNewTodoInTable(todoFormData, todoIndex);
 
   todoIndex =+ 1;
 });
-function insertNewTodoInTable(todoFormData){
-  let todoTableRef = document.getElementById("todo-table");
-  let newTodoRowRef = todoTableRef.insertRow(-1);
-  let newTypeCellRef = newTodoRowRef.insertCell(0);
-  let tableId = todoTableRef.rows.length - 1;
-  newTypeCellRef.textContent = todoTableRef.rows.length - 1;
-  newTypeCellRef = newTodoRowRef.insertCell(1);
-  newTypeCellRef.textContent = todoFormData.get("task-name")
-  newTypeCellRef = newTodoRowRef.insertCell(2);
-  newTypeCellRef.textContent = todoFormData.get("task-type")
-  newTypeCellRef = newTodoRowRef.insertCell(3);
-  newTypeCellRef.textContent = todoFormData.get("task-limit-date")
-  newTypeCellRef = newTodoRowRef.insertCell(4);
-  newTypeCellRef.textContent = todoFormData.get("task-tag")
-  newTypeCellRef = newTodoRowRef.insertCell(5);
-  newTypeCellRef.innerHTML = "<input type='checkbox' name='task-done-" + tableId + "'></input>";
-}
