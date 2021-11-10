8fe00702ed16ee7458285d470ed89ed11ff4a2c1
P4NCH02798 <43188535+P4NCH02798@users.noreply.github.com>
2021-11-10 18:52:04 -0400

    Descripcion funcional

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 let AllTask = Array();
 
-function createTask(nameTask,tipeTask)
-{
-    AllTask.push(nameTask +" , " +tipeTask);
-    return(AllTask);
+function createTask(nameTask, tipeTask) {
+    AllTask.push(nameTask + " , " + tipeTask);
+    return (AllTask);
 }
 
-function returnEtiqueta(etiqueta)
-{
-    let etiquetasConHashtag=getEtiquetaSeparadas(etiqueta);
+function returnEtiqueta(etiqueta) {
+    let etiquetasConHashtag = getEtiquetaSeparadas(etiqueta);
     let stringEtiquetas = etiquetasConHashtag.join(" ");
     return stringEtiquetas;
 }
 
-function getEtiquetaSeparadas(etiquetas)
-{
+function getEtiquetaSeparadas(etiquetas) {
     let separadas = etiquetas.split(',');
-    for (let i = 0; i <separadas.length; i++) {
-        separadas[i]="#"+separadas[i];
+    for (let i = 0; i < separadas.length; i++) {
+        separadas[i] = "#" + separadas[i];
     }
     return separadas;
 }
 
-export function insertNewTodoInTable(todoFormData){
+export function insertNewTodoInTable(todoFormData) {
     let todoTableRef = document.getElementById("todo-table");
     let newTodoRowRef = todoTableRef.insertRow(-1);
     let newTypeCellRef = newTodoRowRef.insertCell(0);
     let tableId = todoTableRef.rows.length - 1;
-    let taskTags=todoFormData.get("task-tag");
-    let tags= returnEtiqueta(taskTags);
+    let taskTags = todoFormData.get("task-tag");
+    let tags = returnEtiqueta(taskTags);
     newTypeCellRef.textContent = todoTableRef.rows.length - 1;
     newTypeCellRef = newTodoRowRef.insertCell(1);
     newTypeCellRef.textContent = todoFormData.get("task-name")
@@ -39,11 +36,14 @@ export function insertNewTodoInTable(todoFormData){
     newTypeCellRef = newTodoRowRef.insertCell(4);
     newTypeCellRef.textContent = tags;
     newTypeCellRef = newTodoRowRef.insertCell(5);
+    newTypeCellRef.textContent = todoFormData.get("task-description");
+    newTypeCellRef = newTodoRowRef.insertCell(6);
     newTypeCellRef.innerHTML = "<input type='checkbox' name='task-done-" + tableId + "'></input>";
-  }
+
+}
 
 function getDate(date) {
     return new Date(date).toISOString().split('T')[0];
 }
 
-module.exports = {createTask, returnEtiqueta, getDate, insertNewTodoInTable};
+module.exports = { createTask, returnEtiqueta, getDate, insertNewTodoInTable };
