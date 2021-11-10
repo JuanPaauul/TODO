6b004d04d92769e5552c97239222f811fac7334e
P4NCH02798 <43188535+P4NCH02798@users.noreply.github.com>
2021-11-10 17:18:03 -0400

    Revert "Merge branch 'master' of https://github.com/JuanPaauul/TODO"
    This reverts commit 95c7923ccc518b5fc667e55330fb7f1d7dbc9838, reversing
    changes made to dd0831664f9c025d48097f8ea40d40a59ed66af8.

--- src/App.js
+++ src/App.js
@@ -8,42 +8,11 @@ function createTask(nameTask,tipeTask)
 
 function returnEtiqueta(etiqueta)
 {
-    let etiquetasConHashtag=getEtiquetaSeparadas(etiqueta);
-    let stringEtiquetas = etiquetasConHashtag.join(" ");
-    return stringEtiquetas;
+    return "#etiqueta1"
 }
-
-function getEtiquetaSeparadas(etiquetas)
-{
-    let separadas = etiquetas.split(',');
-    for (let i = 0; i <separadas.length; i++) {
-        separadas[i]="#"+separadas[i];
-    }
-    return separadas;
-}
-
-export function insertNewTodoInTable(todoFormData){
-    let todoTableRef = document.getElementById("todo-table");
-    let newTodoRowRef = todoTableRef.insertRow(-1);
-    let newTypeCellRef = newTodoRowRef.insertCell(0);
-    let tableId = todoTableRef.rows.length - 1;
-    let taskTags=todoFormData.get("task-tag");
-    let tags= returnEtiqueta(taskTags);
-    newTypeCellRef.textContent = todoTableRef.rows.length - 1;
-    newTypeCellRef = newTodoRowRef.insertCell(1);
-    newTypeCellRef.textContent = todoFormData.get("task-name")
-    newTypeCellRef = newTodoRowRef.insertCell(2);
-    newTypeCellRef.textContent = todoFormData.get("task-type")
-    newTypeCellRef = newTodoRowRef.insertCell(3);
-    newTypeCellRef.textContent = todoFormData.get("task-limit-date")
-    newTypeCellRef = newTodoRowRef.insertCell(4);
-    newTypeCellRef.textContent = tags;
-    newTypeCellRef = newTodoRowRef.insertCell(5);
-    newTypeCellRef.innerHTML = "<input type='checkbox' name='task-done-" + tableId + "'></input>";
-  }
 
 function getDate(date) {
     return new Date(date).toISOString().split('T')[0];
 }
 
-module.exports = {createTask, returnEtiqueta, getDate, insertNewTodoInTable};
+module.exports = {createTask, returnEtiqueta, getDate};
