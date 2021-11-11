b2c0821814d7fe0c7b830e63d1dedcb1db228c80
Jonatan Morales <miky14n@gmail.com>
2021-11-10 23:10:36 -0400

    Se verifica que el nombre de la tarea no este vacia

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,11 @@
 import * as f from "./App.js";
 
-describe("Fechas", () => {
+  describe("Fechas", () => {
     it("deberia retornar la fecha", () => {
       var expectedDate = new Date("2021-11-10").toISOString().split('T')[0];
       expect(f.getDate("2021-11-10")).toEqual(expectedDate);
     });
   });
-
   describe("Etiquetas ingresadas por usuario", () => {
     it("deberia retornar la etiqueta con #", () => {
       expect(f.returnEtiqueta("etiqueta1")).toEqual("#etiqueta1");
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import * as f from "./App.js";
 
 const form = document.querySelector("#Organizador-form");
+
+
 var todoIndex = 0;
 var todoData = {};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  alert("Se agrego tarea");
   let todoFormData = new FormData(form);
-  todoFormData["task-id"]=todoIndex.toString();
-  f.insertNewTodoInTable(todoFormData, todoIndex);
-
+  if(todoFormData.get("task-name") !=""){
+    todoFormData["task-id"]=todoIndex.toString();
+    f.insertNewTodoInTable(todoFormData, todoIndex);
+  }
+  else{
+    alert("Acaba de ingresar un nombre de tarea vacio"+todoFormData.get("task-name")+ " \nPorfavor ingrese un nombre correcto de tarea");
+  }
   todoIndex =+ 1;
 });
