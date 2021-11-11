8b4b7251fd42f90f1a5ac5c20fba757b36ad9281
Jonatan Morales <miky14n@gmail.com>
2021-11-10 23:29:15 -0400

    Se requiere que todos los campos tengan datos

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ var todoData = {};
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let todoFormData = new FormData(form);
-  if(todoFormData.get("task-name") !=""){
-    todoFormData["task-id"]=todoIndex.toString();
-    f.insertNewTodoInTable(todoFormData, todoIndex);
-  }
-  else{
-    alert("Acaba de ingresar un nombre de tarea vacio"+todoFormData.get("task-name")+ " \nPorfavor ingrese un nombre correcto de tarea");
-  }
+  todoFormData["task-id"]=todoIndex.toString();
+  f.insertNewTodoInTable(todoFormData, todoIndex);
   todoIndex =+ 1;
 });
