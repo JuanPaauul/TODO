b816124f28e1cc6fb60bef3d9db3ebd2507b5568
randyrafaelcg <randyrafaelcg@gmail.com>
2021-11-14 22:13:10 -0400

    ui test added

--- src/marcarTareaTermianda-ui.test.js
+++ src/marcarTareaTermianda-ui.test.js
@@ -2,27 +2,29 @@ import fs from "fs";
 describe("Gestor Tareas", () => {
   beforeAll(() => {
     document.body.innerHTML = fs.readFileSync("index.html", "utf8");
-    require("./App.js");
+    require("./index.js");
   });
-
-  /*it("deberia mostrar la tarea creada", () => {
-    const tarea_elem = document.querySelector("#tarea");
-    const boton_elem = document.querySelector("#crear-tarea");
-    const lista_elem = document.querySelector("#lista-tareas");
-
-    tarea_elem.value = "Primera Tarea";
-    boton_elem.click();
-    expect(lista_elem.innerHTML).toEqual("Primera Tarea");
-  });*/
 
   it("Al iniciar no hay nada en la lista de tareas", () => {
     const rows = document.getElementById("todo-table").rows.length;
     expect(rows).toEqual(1);
   });
 
+  it("deberia mostrar la tarea creada", () => {
+    const tarea_elem = document.querySelector("#name-task");
+    const boton_elem = document.querySelector("#botonclick");
+    const lista_elem = document.querySelector("#list-task");
+    tarea_elem.value = "Primera Tarea";
+    boton_elem.click();
+    let tabla = document.getElementById("todo-table").rows;
+    let last = tabla[tabla.length - 1];
+    let cell = last.cells[1];
+    let value = cell.innerHTML
+    expect(value).toEqual("Primera Tarea");
+  });
+
   afterEach(() => {
-    //document.body.innerHTML = fs.readFileSync("index.html", "utf8");
-    const lista_elem = document.querySelector("#todo-table");
+    const lista_elem = document.querySelector("#list-task");
     lista_elem.innerHTML = "";
   });
 });
