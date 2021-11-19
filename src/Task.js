f1e43a7dd3be4776760828ed28feb1f967e6deee
Franklin <franklin.rosembluth@ucb.edu.bo>
2021-11-19 19:59:19 -0400

    Creando la clase Tarea

---
+++ src/Task.js
@@ -0,0 +1,12 @@
+class Task {
+    constructor(id, name, type, limitdate, label, description) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.limitdate = limitdate;
+        this.label = label;
+        this.description = description;
+    }
+}
+
+export default {Task}
